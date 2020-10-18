3aa89fc7f893ec370657b879dd840e92eaf04d74
Dongjun Jeong <dkdlel1087@naver.com>
2020-10-18 23:56:51 +0900

    TIL: JS

---
+++ NomadCoder/ChromeApp/index.js
@@ -0,0 +1,28 @@
+console.log("Im Working. Im JS. Im beautiful. Im worth it");
+
+// let & const
+let a = 221;
+let b = a-5;
+a = 4;
+console.log(b, a);
+// 웬만하면 const, 꼭 필요할 때만 let.
+
+//String
+const what = "Nicolas";
+
+//Boolean
+const wat = true;
+
+//Number
+const num = 666;
+
+//Float
+const float = 66.6;
+
+//Array
+const Something = "Some";
+const daysOfWeek = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun",true,Something]
+console.log(daysOfWeek[daysOfWeek.length-1]);
+
+//Object(python의 dictionary)
+const nicoInfo = {};
