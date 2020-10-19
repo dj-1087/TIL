2b2ddee656f7bb8edb08e3cd4f490dbe8e9cb52b
Dongjun Jeong <dkdlel1087@naver.com>
2020-10-20 02:26:59 +0900

    TIL

--- NomadCoder/ChromeApp/weather.js
+++ NomadCoder/ChromeApp/weather.js
@@ -1,5 +1,19 @@
+const weather = document.querySelector(".js-weather")
+
 const API_KEY = "04226408fb83e260d5adc928e5df64b1";
 const COORDS = 'coords';
+
+function getWeather(lat, lng) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(json) {
+            const temperature = json.main.temp;
+            const place = json.name;
+            weather.innerText = `${temperature} @ ${place}`;
+        });
+}
 
 function saveCoords(coordsObj) {
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -10,6 +24,7 @@ function handleGeoSuccess(position) {
     const longitude = position.coords.longitude;
     const coordsObj = { latitude, longitude };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 function handleGeoError() {
     console.log(`Can't access geolocation.`);
@@ -24,7 +39,8 @@ function loadCoords() {
     if(loadCoords === null) {
         askForCoords();
     }else {
-        //getWeather
+        const parseCoords = JSON.parse(loadCoords);
+        getWeather(parseCoords.latitude,parseCoords.longitude);
     }
 }
 
