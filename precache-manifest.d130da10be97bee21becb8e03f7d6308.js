e15b370e1ea3c70516bb1a76a1e2ac2f665dbf57
Dongjun Jeong <dkdlel1087@naver.com>
2020-10-28 17:05:09 +0900

    Updates

---
+++ precache-manifest.d130da10be97bee21becb8e03f7d6308.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "927e7662ccb77f982a4d7daf729f42bb",
+    "url": "/TIL/NomadCoder/MovieApp/movie_app/index.html"
+  },
+  {
+    "revision": "5520e4a38fb4c6becbfe",
+    "url": "/TIL/NomadCoder/MovieApp/movie_app/static/css/main.64cd11d8.chunk.css"
+  },
+  {
+    "revision": "380526f9da90ef891229",
+    "url": "/TIL/NomadCoder/MovieApp/movie_app/static/js/2.44318e77.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/TIL/NomadCoder/MovieApp/movie_app/static/js/2.44318e77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5520e4a38fb4c6becbfe",
+    "url": "/TIL/NomadCoder/MovieApp/movie_app/static/js/main.5d0486ac.chunk.js"
+  },
+  {
+    "revision": "61849a22ef70a84f0da9",
+    "url": "/TIL/NomadCoder/MovieApp/movie_app/static/js/runtime-main.b1ad4e25.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.903aaaa2d09764da9787633fb5b701a4.js"
+  "/TIL/NomadCoder/MovieApp/movie_app/precache-manifest.d130da10be97bee21becb8e03f7d6308.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/TIL/NomadCoder/MovieApp/movie_app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
